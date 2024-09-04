aea879221ef1707ecca3f9394130843cabcb1def
WolftheE <dan.com.au123@outlook.com.au>
2024-09-05 09:29:18 +1000

    Update 2d part

--- scripts/other_scripts/gravity_component.gd
+++ scripts/other_scripts/gravity_component.gd
@@ -6,15 +6,8 @@ extends Node
 
 var is_falling: bool = false
 
-func handle_gravity(body: CharacterBody2D, dalta: float) -> void:
+func handle_gravity(body: CharacterBody2D, delta: float) -> void:
 	if not body.is_on_floor():
 		body.velocity.y += gravity * delta
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+	is_falling = body.velocity.y > 0 and not body.is_on_floor()
